5008f6b52ebce1a82db0b15bc8db74f1bd7fa093
opddinx <opddinx@gmail.com>
2024-06-28 04:06:27 +0900

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,23 @@
-if (typeof window !== 'undefined') {
-    import("./utils/EventBus")
-    import("./modules/WebGL")
-    if (!window.isDev) window.isDev = false;
+async function initModules() {
+    if (typeof window !== 'undefined') {
+        try {
+            const [EventBusModule, WebGLModule] = await Promise.all([
+                import("./utils/EventBus"),
+                import("./modules/WebGL")
+            ]);
 
-    window.EventBus = EventBus;
+            if (!window.isDev) window.isDev = false;
 
-    const webglMng = new WebGL({
-        $wrapper: document.body
-    });
+            window.EventBus = EventBusModule.default || EventBusModule;
+
+            const WebGL = WebGLModule.default || WebGLModule;
+            const webglMng = new WebGL({
+                $wrapper: document.body
+            });
+        } catch (error) {
+            console.error("Error importing modules:", error);
+        }
+    }
 }
+
+initModules();
